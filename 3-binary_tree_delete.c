816ad2f6b1c91a6223cd55d25fadebd81432aaf7
cynthiaclaire4 <cynthiaclaire@DESKTOP-2RN3GRE.localdomain>
2022-08-25 21:09:36 +0100

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: tree to delete
+ * binary_tree_delete - free tree
+ * @tree: tree to free
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return;
 	binary_tree_delete(tree->right);
 	binary_tree_delete(tree->left);
 	free(tree);
-}
+}}
