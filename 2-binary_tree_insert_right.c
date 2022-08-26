1e4cdfd8218f9f1621ba7052f0d2e398bcf2503e
Ifeanyik <jax64720@gmail.com>
2022-08-25 17:25:15 -0700

    Fix edge case failure

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *old_right_child;
 
+	if (!parent)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
@@ -19,8 +21,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = NULL;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (!parent)
-		return (NULL);
 	if (!parent->right)
 	{
 		parent->right = new_node;
